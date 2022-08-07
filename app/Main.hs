8e77943877a48affb5e0207cecc2bcbcc494983a
Nannk <serf.jp@gmail.com>
2022-08-07 16:05:56 +0200

    Main.hs

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,13 @@
 module Main where
 
-import Lib
+import Graphics.Image
+import Brick
+
+data App state event resource_name = 
+    {appDraw :: state -> [Widget resource_name]
+
+
+someFunc = print "hello world"
 
 main :: IO ()
 main = someFunc
