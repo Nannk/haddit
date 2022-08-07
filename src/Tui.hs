27654823d1fb7f30df1bb6bc43af5a7d6470771c
Nannk <serf.jp@gmail.com>
2022-08-07 20:27:22 +0200

    Plan for ui elements

--- src/Tui.hs
+++ src/Tui.hs
@@ -1,4 +1,5 @@
 module Tui where
+import Cursor.Simple.List.NonEmpty
 
 import Brick.AttrMap
 import Brick.Main
@@ -11,11 +12,26 @@ import System.Posix.Internals (statGetType)
 import Graphics.Vty (standout)
 import Network.Curl
 
--- str 
--- vBox 
-
+    -- Data Section 
+-- test data
 startPageUI :: String
 startPageUI ="Search for a subreddit"
+
+        -- every comment in this section is tui related, no web stuff
+    -- UIInputs: user -> pc 
+-- 1 Hotkeys 
+quitKey = 'q'
+nextPostKey = 'l' 
+previousPostKey = 'k'
+
+-- 2 subreddit/post/user search (Form)
+stringInputForm :: Form String  (*)
+
+-- 3 choose an element from the list (subreddit/post/comment) (Cursor)
+    -- UIOutputs
+-- 1 List of interactible elements (subreddits/posts)
+-- 2 List of elements (comments)
+-- 3 Images (embedded in posts)
 
 tui :: IO()
 tui = do
@@ -23,13 +39,19 @@ tui = do
     endState <- defaultMain tuiApp initialState
     print endState
 
-data TuiState = TuiState
-    {tuiStateGreeting :: [String]}
+data TuiState = TuiState --datastructure of all tui states
+    {
+    tuiStateGreeting :: [String]
+    
+    }
     deriving(Show, Eq)
 
+-- Just a placeholder from the tutorial for convinience
+    -- probably wouldnt need it 
 type ResourceName = String 
 
 
+-- the main App that conrols the resources 
 tuiApp :: App TuiState e ResourceName
 tuiApp = 
     App
